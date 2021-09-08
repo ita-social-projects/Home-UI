21cbc510c3e78308f12eedce35c1096dbb9c1c71
markin <markuwas@gmail.com>
2021-09-08 15:14:26 +0300

    PrimeVue moved to global styles

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import { createApp } from 'vue';
 import router from './router';
 import store from './store';
 
-import PrimeVue from 'primevue/config';
-
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
-app.use(PrimeVue);
 app.mount('#app');
