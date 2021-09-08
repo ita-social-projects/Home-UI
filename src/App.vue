715458e2b9182af6b925d3031e2bda79b50ef6f0
markin <markuwas@gmail.com>
2021-09-08 13:18:07 +0300

    Testes code removed

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <template>
   <h1>Home</h1>
-  <div>
-    {{text}}
-    <InputText v-model="text" />
-    <Button 
-    label="Check" 
-    icon="pi pi-check" 
-    iconPos="left" 
-    class="p-button-raised p-button-rounded" 
-    @click="showMessage(text)" />
-
-    <span class="p-buttonset">
-      <Button label="Save" icon="pi pi-check" />
-      <Button label="Delete" icon="pi pi-trash" />
-      <Button label="Cancel" icon="pi pi-times" />
-  </span>
-  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      text: 'Some text'
-    }
-  },
-  methods: {
-    showMessage(text) {
-      console.log(text)
-    }
-  }
-}
-</script>
-
-
 <style lang="scss">
-  #app {
-    margin-top: 70px;
-    text-align: center;
-  }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import store from './store';
 import App from './App.vue';
 
 import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
 
 import 'primevue/resources/themes/saga-blue/theme.css'; 
 import 'primevue/resources/primevue.min.css';          
@@ -17,6 +15,4 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(PrimeVue);
-app.component('Button', Button);
-app.component('InputText', InputText);
 app.mount('#app');
