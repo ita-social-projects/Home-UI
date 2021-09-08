41c01a35d8a778344b1a3fac3622f68f1dfcac70
markin <markuwas@gmail.com>
2021-09-08 12:38:26 +0300

    Installed PrimeVue, tested Button and InputText componets, icons, styles

--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import store from './store';
 
 import App from './App.vue';
 
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+import 'primevue/resources/themes/saga-blue/theme.css'; 
+import 'primevue/resources/primevue.min.css';          
+import 'primeicons/primeicons.css';
+
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('InputText', InputText);
 app.mount('#app');
