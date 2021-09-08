0f6b4eb660143102745d1daf02313d78795b3c35
markin <markuwas@gmail.com>
2021-09-08 14:13:59 +0300

    Testes code removed

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ import store from './store';
 
 import App from './App.vue';
 
-import PrimeVue from 'primevue/config';
-
-import 'primevue/resources/themes/saga-blue/theme.css'; 
-import 'primevue/resources/primevue.min.css';          
-import 'primeicons/primeicons.css';
-
 const app = createApp(App);
 
 app.use(store);
