63cff85de243a9e2749d34c7ced3cd75be40a0f1
markin <markuwas@gmail.com>
2021-09-08 17:43:35 +0300

    PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import store from './store'
 
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config'
+
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
+app.use(PrimeVue)
 app.mount('#app')
