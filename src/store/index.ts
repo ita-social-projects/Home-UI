a260a769f77e6861f3f01f32525778bdc8ddf867
asteof <kirill.brezhnev02@gmail.com>
2021-09-08 21:10:14 +0300

    Done #16 - Enable strict mode on vuex store production build Basic vuex configuration in TypeScript

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,8 @@
 import { createStore } from 'vuex'
+import { HTTP } from '@/core/api/http-common'
 
 export default createStore({
+  strict: process.env.NODE_ENV !== 'production',
   state: {},
   mutations: {},
   actions: {},
