29a5e31887766d923cb016c05f8b9fef5fb6c4a8
markin <markuwas@gmail.com>
2021-09-09 11:45:36 +0300

    Done

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+}
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import {
+  createRouter,
+  createWebHistory,
+  RouteRecordRaw,
+} from 'vue-router'
+import Home from '@/views/Home.vue';
+
+const routes: RouteRecordRaw[] = [
+  // initial route; should be customized or removed
+  {
+    path: '/',
+    component: Home,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
