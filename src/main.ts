0f84d7a7af5c0d327bed89663a72ac3caba887b3
asteof <kirill.brezhnev02@gmail.com>
2021-09-09 15:18:50 +0300

    #16 - Enable strict mode on vuex store production build Third fix of unnecessary code from other PR Basic vuex configuration in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import router from './router'
 import store from './store'
 
 import App from './App.vue'
-import { HTTP } from './core/api/http-common'
 
 const app = createApp(App)
 
-app.config.globalProperties.$http = HTTP
 app.use(store)
 app.use(router)
 app.mount('#app')
