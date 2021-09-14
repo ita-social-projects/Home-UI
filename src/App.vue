b44abaf79e8c2a14aaa02a42824b42599307851c
asteof <kirill.brezhnev02@gmail.com>
2021-09-14 12:38:25 +0300

    TS2769 reproduction code TS2769: No overload matches this call.

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,24 @@
   <router-view> </router-view>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'dad',
+  created() {
+    this.getUsers()
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    getUsers() {
+      // this.$http.get('/users').then(console.log).catch(console.log)
+      console.log(this.$store)
+    },
+  },
+})
+</script>
+
 <style lang="scss"></style>
