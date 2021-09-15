8a23fb39cfe1b647c84d930d6cde9e96baba32ff
asteof <kirill.brezhnev02@gmail.com>
2021-09-15 19:54:47 +0300

    added global styles style reset

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { HTTP } from '@/core/api/http-common';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 
-import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/themes/tailwind-light/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import '@/styles/style.scss';
+
 const app = createApp(App);
 
 app.config.globalProperties.$http = HTTP;
