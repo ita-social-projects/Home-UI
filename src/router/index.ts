0cb5ef1033310f16553211307b42a372041aa59b
what-the-hub <iamthenight.yeah@gmail.com>
2021-09-17 00:44:22 +0300

    #22 Create page, add styles, some validation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue';
-import UserRegistration from '../views/UserRegistration.vue';
+import UserRegistration from '@/views/UserRegistration.vue';
 
 const routes: RouteRecordRaw[] = [
   // initial route; should be customized or removed
