6513cb0ebecdd24c011a17075f6f04599a54bbdc
asteof <kirill.brezhnev02@gmail.com>
2021-09-17 18:49:01 +0300

    resolve conflicts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import StartPage from '@/views/StartPage.vue';
 import RegisterCooperation from '@/views/RegisterCooperation.vue';
+import UserLogin from '@/views/UserLogin.vue';
 
 const routes: RouteRecordRaw[] = [
   // initial route; should be customized or removed
   {
     path: '/',
     component: StartPage,
+  },
+  {
+    path: '/login',
+    component: UserLogin,
   },
   {
     path: '/register-cooperation',
