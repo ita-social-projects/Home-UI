465bd35592bf0d132ff4f740e18a7293542d4bc1
markin <markuwas@gmail.com>
2021-09-24 09:00:54 +0300

    #23 Resolve conflicts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,14 +3,8 @@ import { cooperationModule } from '@/store/cooperationModule';
 
 export default createStore({
   strict: process.env.NODE_ENV !== 'production',
-  state: {
-    user: null,
-  },
-  mutations: {
-    setUser(state, payload) {
-      state.user = payload;
-    },
-  },
+  state: {},
+  mutations: {},
   actions: {},
   modules: {
     c: cooperationModule,
