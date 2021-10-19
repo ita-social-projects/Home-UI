0641835c7112250b9da0bc53c64e5cf74bfdedf3
asteof <kirill.brezhnev02@gmail.com>
2021-10-19 19:31:40 +0300

    save changes

---
+++ src/store/polls/poll.model.ts
@@ -0,0 +1,20 @@
+import { UserDataInterface, ContactInterface } from '@/store/user/types';
+
+export class UpdateUserModel {
+  public registration_token: string;
+  public first_name: string;
+  //public middle_name: string;
+  public last_name: string;
+  public email: string;
+  public password: string;
+  public contacts: ContactInterface[];
+
+  constructor(userData: UserDataInterface) {
+    this.registration_token = userData.registrationToken;
+    this.first_name = userData.firstName;
+    this.last_name = userData.lastName;
+    this.email = userData.email;
+    this.password = userData.password;
+    this.contacts = userData.contacts;
+  }
+}
