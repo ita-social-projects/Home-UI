42611ae374ac25d932157ca6cf6d26336f70ff7b
tatianaKonovalova <tatiana.konovalova24@gmail.com>
2021-12-13 10:59:09 +0200

    initialized store

---
+++ src/store/invitations/types.ts
@@ -0,0 +1,46 @@
+import { ActionContext } from 'vuex';
+import { RootStateInterface } from '@/store/types';
+
+export enum InvitationsMutationsEnum {
+  GET_ALL_INVITATIONS = 'GET_ALL_INVITATIONS',
+}
+
+export enum InvitationsActionsEnum {
+  GET_ALL_INVITATIONS = 'GET_ALL_INVITATIONS',
+}
+
+export interface InvitationInterface {
+  email: string;
+  address: AddressInterface;
+  status: string;
+  id: number;
+}
+
+export interface AddressInterface {
+  street: string;
+  houseBlock: string;
+  houseNumber: string;
+}
+
+export interface InvitationsStateInterface {
+  invitations: Array<InvitationInterface> | null;
+}
+
+export type Mutations<S = InvitationsStateInterface> = {
+  [InvitationsMutationsEnum.GET_ALL_INVITATIONS](state: S, payload: Array<InvitationInterface>): void;
+};
+
+export interface Actions {
+  [InvitationsActionsEnum.GET_ALL_INVITATIONS](
+    { commit }: AugmentedActionContext,
+    payload: Array<InvitationInterface>
+  ): void;
+}
+
+export type Getters<S = InvitationsStateInterface> = {
+  getInvitations(state: S): Array<InvitationInterface> | null;
+};
+
+export type AugmentedActionContext = {
+  commit<K extends keyof Mutations>(key: K, payload: Parameters<Mutations[K]>[1]): ReturnType<Mutations[K]>;
+} & Omit<ActionContext<InvitationsStateInterface, RootStateInterface>, 'commit'>;
