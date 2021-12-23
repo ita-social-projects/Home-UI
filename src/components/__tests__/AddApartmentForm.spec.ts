c4f65df4fffe45d128c0bb69e14e283a0b011ce4
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-12-23 22:51:13 +0200

    test coverage 85%

--- src/components/__tests__/AddApartmentForm.spec.ts
+++ src/components/__tests__/AddApartmentForm.spec.ts
@@ -1,4 +1,4 @@
-import { mount, VueWrapper } from '@vue/test-utils';
+import { mount, VueWrapper, shallowMount } from '@vue/test-utils';
 import AddApartmentForm from '@/components/AddApartmentForm.vue';
 import { ComponentPublicInstance } from 'vue';
 
@@ -9,35 +9,150 @@ const setup = async (id: string, value: string, wrapper: VueWrapper<ComponentPub
 };
 
 describe('AddApartmentForm.vue', () => {
+  // test('vuex', async () => {
+  //   const $store = {
+  //     state: {
+  //       id: 5,
+  //       number: 10,
+  //       area: 50,
+  //     },
+  //     commit: jest.fn(),
+  //   };
+  //   const wrapper = mount(AddApartmentForm, {
+  //     global: {
+  //       mocks: {
+  //         $store,
+  //       },
+  //     },
+  //   });
+
+  //   await wrapper.find('#save-button').trigger('click');
+  //   expect($store.commit).toHaveBeenCalled();
+  // });
+
   it('should exist', () => {
     const wrapper = mount(AddApartmentForm);
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it('should set the value in the email field', async () => {
+  it('should set the value in the apartment_number field', async () => {
     const wrapper = mount(AddApartmentForm);
-    const input = wrapper.find('#email');
-    await input.setValue('email@email.com');
-    expect(input.element._value).toBe('email@email.com');
+    const input = wrapper.find('#apartment_number');
+    await input.setValue('123');
+    expect(input.element._value).toBe('123');
   });
 
-  it('should set the value in the edrpou field', async () => {
+  it('should set the value in the apartment_area field', async () => {
     const wrapper = mount(AddApartmentForm);
-    const input = wrapper.find('#edrpou');
-    await input.setValue('12345678');
-    expect(input.element._value).toBe('12345678');
+    const input = wrapper.find('#apartment_area');
+    await input.setValue('123');
+    expect(input.element._value).toBe('123');
   });
 
-  it('should pass the validation - email field', async () => {
+  it('should pass the validation - apartment_number field', async () => {
     const wrapper = mount(AddApartmentForm);
-    const input = wrapper.find('#email');
-    await input.setValue('email@email.com');
-    expect(wrapper.find('small#email-help').exists()).toBe(false);
+    const input = wrapper.find('#apartment_number');
+    await input.setValue('123');
+    expect(wrapper.find('small#apartment_number_help').exists()).toBe(false);
   });
 
-  it('should fail the validation - email field [is required]', async () => {
+  it('should pass the validation - apartment_area field', async () => {
     const wrapper = mount(AddApartmentForm);
-    await setup('#email', '', wrapper);
-    expect(wrapper.find('small#email-help').text()).toBe("Це обов'язкове поле");
+    const input = wrapper.find('#apartment_area');
+    await input.setValue('123');
+    expect(wrapper.find('small#apartment_area_help').exists()).toBe(false);
+  });
+
+  it('should fail the validation - apartment_number field [is required]', async () => {
+    const wrapper = mount(AddApartmentForm);
+    await setup('#apartment_number', '', wrapper);
+    expect(wrapper.find('small#apartment_number_help').text()).toBe("Це обов'язкове поле");
+  });
+
+  it('should fail the validation - apartment_area field [is required]', async () => {
+    const wrapper = mount(AddApartmentForm);
+    await setup('#apartment_area', '', wrapper);
+    expect(wrapper.find('small#apartment_area_help').text()).toBe("Це обов'язкове поле");
+  });
+
+  it('apartment-saved emit test', async () => {
+    const wrapper = mount(AddApartmentForm);
+
+    wrapper.vm.$emit('apartment-saved');
+    //wrapper.vm.$emit('apartment-saved', 123);
+    await wrapper.vm.$nextTick(); // Wait until $emits have been handled
+    // assert event has been emitted
+    expect(wrapper.emitted('apartment-saved')).toBeTruthy();
+    // assert event count
+    //expect(wrapper.emitted('apartment-saved').length).toBe(2);
+  });
+
+  it('cancel-editing emit test', async () => {
+    const wrapper = mount(AddApartmentForm);
+    wrapper.vm.$emit('cancel-editing');
+    //wrapper.vm.$emit('cancel-editing', 123);
+    await wrapper.vm.$nextTick(); // Wait until $emits have been handled
+    // assert event has been emitted
+    expect(wrapper.emitted('cancel-editing')).toBeTruthy();
+    // assert event count
+    //  expect(wrapper.emitted('apartment-saved').length).toBe(2);
+  });
+
+  it('props', () => {
+    const wrapper = mount(AddApartmentForm, {
+      propsData: {
+        houseId: 5,
+      },
+    });
+    expect(wrapper.props().houseId).toBe(5);
+    expect(wrapper.props('houseId')).toBe(5);
+  });
+
+  it('test method cancelEditing', async () => {
+    const cancelEditing = jest.spyOn(AddApartmentForm.methods, 'cancelEditing');
+    const wrapper = mount(AddApartmentForm);
+    wrapper.find('#cancel-button').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(cancelEditing).toBeCalled();
+  });
+
+  it('test method saveApartment', async () => {
+    const saveApartment = jest.spyOn(AddApartmentForm.methods, 'saveApartment');
+    const wrapper = mount(AddApartmentForm);
+    wrapper.find('#save-button').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(saveApartment).toBeCalled();
+  });
+
+  // ??????
+  test('emit check test', async () => {
+    const wrapper = mount(AddApartmentForm);
+    await wrapper.find('#save-button').trigger('click');
+    console.log(wrapper.emitted());
+  });
+
+  it('test method saveApartment and clear', async () => {
+    // const saveApartment = jest.spyOn(AddApartmentForm.methods, 'saveApartment');
+    const wrapper = mount(AddApartmentForm);
+    wrapper.find('#apartment_number').setValue('123');
+    wrapper.find('#apartment_area').setValue(555);
+    wrapper.find('#save-button').trigger('click');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('#apartment_area').text()).toBe('');
+    expect(wrapper.find('#apartment_number').text()).toBe('');
+  });
+
+  it('test method resetApartmentDataFields', async () => {
+    const mockdata = {
+      apartmentData: {
+        apartmentNumber: '666',
+        apartmentArea: 666,
+      },
+    };
+    const resetApartmentDataFields = jest.spyOn(AddApartmentForm.methods, 'resetApartmentDataFields');
+    const wrapper = mount(AddApartmentForm);
+
+    await wrapper.vm.$nextTick();
+    expect(resetApartmentDataFields).toHaveBeenCalledWith(mockdata);
   });
 });
